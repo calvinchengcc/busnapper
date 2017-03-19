f0929376476bae956ce87e712f645b71f2e8bfee
Calvin Cheng <calvin.cheng.cc@outlook.com>
2017-03-18 19:06:17 -0700

    Add elementary markers to map

--- index.ios.js
+++ index.ios.js
@@ -13,19 +13,67 @@ import {
 } from 'react-native';
 import MapView from 'react-native-maps';
 
+const STATUE_OF_LIBERTY = {
+  latitude: 40.6892,
+  longitude: -74.0445
+};
+
+const EMPIRE_STATE_BUILDING = {
+  latitude: 40.7484,
+  longitude: -73.9857
+};
+
+const BLOOMBERG_HQ = {
+  latitude: 40.7620,
+  longitude: -73.9680
+};
+
+const CENTRAL_PARK = {
+  latitude: 40.7829,
+  longitude: -73.9654
+};
 
 export default class busnapper extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      busStopMarkers: [
+        {
+          id: 61218,
+          coordinate: STATUE_OF_LIBERTY
+        },
+        {
+          id: 81524,
+          coordinate: EMPIRE_STATE_BUILDING
+        },
+        {
+          id: 1512,
+          coordinate: BLOOMBERG_HQ
+        },
+        {
+          id: 362,
+          coordinate: CENTRAL_PARK
+        }
+      ]
+    };
+  }
+
   render() {
     return (
-    <View style={{ position: 'relative', height: 500 }}>
-     <MapView
-        style={{flex:1}}
-        initialRegion={{
-          latitude: 40.6892,
-          longitude: -74.0445,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
-        }}/>
+      <View style={{ position: 'relative', height: 500 }}>
+        <MapView
+          style={{flex:1}}
+          initialRegion={{
+            latitude: 40.6892,
+            longitude: -74.0445,
+            latitudeDelta: 0.08,
+            longitudeDelta: 0.08,
+          }}>
+          {this.state.busStopMarkers.map(busStopMarker => (
+            <MapView.Marker coordinate={busStopMarker.coordinate} />
+          ))}
+        </MapView>
       </View>
     );
   }
